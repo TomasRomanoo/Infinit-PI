c2e2adf78ec6b119e7d7eaeeddf945f0ae51717e
Castro <castrojonathand@gmail.com>
2023-08-10 00:38:27 -0300

    otra prueba mas

--- next.config.js
+++ next.config.js
@@ -1,13 +1,34 @@
-module.exports = {
+// module.exports = {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: 'assets.example.com',
+//           port: '',
+//           pathname: 'via.placeholder.com',
+//         },
+//       ],
+//     },
+//   }
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'assets.example.com',
-          port: '',
-          pathname: 'via.placeholder.com',
-        },
-      ],
-    },
-  }
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'assets.example.com',
+            port: '',
+            pathname: 'via.placeholder.com',
+          },
+        ],
+      },
+    typescript: {
+      ignoreBuildErrors: true,
+    }
+
+
+}
+module.exports = nextConfig
+
   
