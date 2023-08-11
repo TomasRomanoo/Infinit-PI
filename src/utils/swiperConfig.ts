e2a2b0ae4b6f9ce117db791ec9087dc76117f416
tadeogavensky <tadeogavensky.email@gmail.com>
2023-08-11 12:59:17 -0300

    details of gallery

--- src/utils/swiperConfig.ts
+++ src/utils/swiperConfig.ts
@@ -36,11 +36,11 @@ const swiperConfig: SwiperConfig = {
     },
     1024: {
       slidesPerView: 1,
-      spaceBetween: 30,
+      spaceBetween: 10,
     },
     1280: {
       slidesPerView: 2,
-      spaceBetween: 30,
+      spaceBetween: 10,
     },
   },
 };
