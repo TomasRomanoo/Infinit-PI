1eb1c1ea8eeb1cb66c90488889c729a171aa568b
Castro <castrojonathand@gmail.com>
2023-08-31 01:47:02 -0300

    availability avanced2.0

--- src/app/api/availability/route.js
+++ src/app/api/availability/route.js
@@ -15,11 +15,14 @@ export async function GET() {
         });
 
         // aca traigo las fechas en que el auto se reserva
-        // const entryDates = availability.map(item => item.checkin_date)    
+        const entryDates = availability.map(item => ({
+            checkin_date: item.checkin_date,
+            checkout_date: item.checkout_date
+        })); 
 
     
         console.log("el metodo Get se esta ejecutando");
-        return NextResponse.json( availability,{ status: 200, message: "Todo está OK" });
+        return NextResponse.json( entryDates,{ status: 200, message: "Todo está OK" });
     } catch (error) {
       // En caso de que ocurra un error, registra el error en la consola y responde con un código de estado 500 y un mensaje de error
         console.error("Error fetching availability: ", error);
