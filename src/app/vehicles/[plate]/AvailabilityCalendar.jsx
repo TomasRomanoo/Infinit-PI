f2a5a78d3c25e5e3c8ee52ba17a53ff7ba2a54a0
Castro <castrojonathand@gmail.com>
2023-09-04 00:10:32 -0300

    us23 finished

--- src/app/vehicles/[plate]/AvailabilityCalendar.jsx
+++ src/app/vehicles/[plate]/AvailabilityCalendar.jsx
@@ -3,11 +3,15 @@ import React, { useState } from "react";
 import { useEffect } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import Swal from 'sweetalert2';
+
+
 
 const AvailabilityCalendar = ({idvehicle}) => {
   const [dateRange, setDateRange] = useState();  
-  const [AvailableDates, setAvailableDates] = useState()  
-  const [error, setError] = useState()
+  const [AvailableDates, setAvailableDates] = useState()   
+  const [Error, setError] = useState()
+  const [isError, setIsError] = useState(false)
 
   const fetchAvailability = async () => {
     try {
@@ -15,16 +19,26 @@ const AvailabilityCalendar = ({idvehicle}) => {
         const response = await axios(`/api/availability/${idvehicle}`);
         console.log("res.data",response.data);        
         const powerRange = response.data.map((dateRange) => ({
+          
           start: new Date(dateRange.start.split("T")[0]), 
-          end: new Date(dateRange.end.split("T")[0])    
-
+          end: new Date(dateRange.end.split("T")[0]),     
+          
         }));
-        setAvailableDates(powerRange);        
+        setAvailableDates(powerRange); 
+        setIsError(false)       
       }    
-    } catch (error) {
-      console.error("Error fetching availability: ", error);
-      setError("Unable to fetch availability. Please try again later.");       
-    }      
+      } catch (error) {
+          console.error("Error fetching availability: ", error);
+          setError("Unable to fetch availability. Please try again later.");
+          setIsError(true)
+
+          Swal.fire({
+            icon: 'error',
+            title: 'Unable to fetch availability. Please try again later...',
+            
+          });
+        }   
+      
   } 
   useEffect(() => {
     fetchAvailability()   
@@ -34,16 +48,23 @@ const AvailabilityCalendar = ({idvehicle}) => {
   return (
     <div className="availability-calendar-container">
       <div className="availability-calendar">
-        <div className="calendar-label">Select Dates:</div>
-        <DatePicker
+        <div className="calendar-label">Display of availability and busy dates:
+        </div>
+        {
+          isError ? 
+          <p>Please try again later...</p>
+          :
+          <DatePicker
           selected={dateRange}
-          onChange={(date) => setDateRange(date)}                 
+          onChange={(date) => setDateRange(date)}                     
           excludeDateIntervals={ AvailableDates}
           monthsShown={2}
-          withPortal
-          showIcon          
-          placeholderText="show availability" 
+          withPortal          
+          showIcon         
+          placeholderText="show availability"
         />
+        }       
+        
       </div>
     </div>
   );
