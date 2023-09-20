b19f4ff0dbb3d378388dd4a7e1f80bd564dd7b98
elxro <monetaaaaas@gmail.com>
2023-09-20 04:00:34 -0300

    ending styles

--- src/app/reservation/[id]/confirm/page.js
+++ src/app/reservation/[id]/confirm/page.js
@@ -9,8 +9,8 @@ import Image from "next/image";
 import Success from "./success";
 import { HiCalendarDays } from "react-icons/hi2";
 import Slider from "react-slick";
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function ConfirmationPage({ params }) {
     const userContext = useContext(UserContext);
@@ -70,34 +70,37 @@ function ConfirmationPage({ params }) {
         var dateObject1 = new Date(date1);
         var dateObject2 = new Date(date2);
         var differenceInMilliseconds = dateObject2 - dateObject1;
-        var differenceInDays = (differenceInMilliseconds / (1000 * 60 * 60 * 24))+1;
-        asd= differenceInDays
+        var differenceInDays = (differenceInMilliseconds / (1000 * 60 * 60 * 24)) + 1;
+        asd = differenceInDays
         return differenceInDays;
     }
 
 
     const settings = {
-        customPaging: function(i) {
-          return (
-            <a>
-              <img src={vehicle.images?.length > 1 ? vehicle.images[1]?.url : {i}} />
-            </a>
-          );
+        customPaging: function (i) {
+            return (
+                <a>
+                    <img src={vehicle.images?.length > 1 ? vehicle.images[i]?.url : { i }} style={{ width: '100px', height: 'auto', objectFit: 'cover' }} />
+                </a>
+            );
         },
         dots: true,
         dotsClass: "slick-dots slick-thumb",
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
-      };
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true
+    };
     return (
         <div>
             {saved === false ? (<div>
                 <h1 className="m-5 text-3xl font-bold">Rental Confirmation</h1>
                 <div className="shadow-lg p-10 bg-white rounded-xl">
                     <h2 className="m-5 text-xl">This is the last step. Check if the information is correct before confirming!</h2>
-                    <div className="flex justify-between my-12">
+                    <div className="flex justify-around my-12">
                         <div className="w-5/12">
                             <div className="p-4 my-3" >
                                 <h1 className="font-bold text-lg mb-2">Reservator data</h1>
@@ -127,7 +130,7 @@ function ConfirmationPage({ params }) {
                                     </div>
                                 </div>
 
-                                <hr className=" rounded border-2"/>
+                                <hr className=" rounded border-2" />
                                 <div className="flex w-full justify-around my-10">
                                     <div className="flex items-center" >
                                         <HiCalendarDays className="text-5xl text-primary" />
@@ -148,7 +151,7 @@ function ConfirmationPage({ params }) {
                                         </div>
                                     </div>
 
-                                        <div class=" h-12 border-2 border-slate-300 self-center mx-2 rounded"></div>
+                                    <div class=" h-12 border-2 border-slate-300 self-center mx-2 rounded"></div>
 
                                     <div className="flex items-center mr-10 opacity-80" >
                                         <HiCalendarDays className="text-5xl text-primary" />
@@ -170,56 +173,62 @@ function ConfirmationPage({ params }) {
                                     </div>
                                 </div>
                             </div>
-                            
-                            <hr className=" rounded border-2"/>
+
+                            <hr className=" rounded border-2" />
 
                             <div className="flex flex-col my-5 items-center">
                                 <p className="font-extrabold text-6xl text-primary my-5"> Total ${(calculateDateDifference(reservation.start, reservation.end)) * vehicle.price_per_day} </p>
                                 <p className="text-slate-500"> (For {asd} days reserve)</p>
                             </div>
+
+                            <div className="self-center">
+                                        <button className="bg-primary text-white font-bold rounded-xl  w-full py-3 m-4"
+                                            onClick={handleReservationClick}>   Confirm
+                                        </button>
+                                    </div>
                         </div>
-                        <div className="w-6/12">
-                            <div className=" flex flex-col ">
-                                        <h1 className="font-bold text-lg mb-2">Vehicle data</h1>
-
-                                        <div className="my-2">
-                                            <p>{vehicle.model?.brand?.name.toUpperCase()} {vehicle.model?.name.toUpperCase()}</p>
-                                        </div>
-                                <div>
-                                    {/* Main Image */}
-                                    <div className="w-full hover:brightness-75 transition-all duration-200 self-stretch flex-column">
-                                        {/* <Image
-                                            className="object-contain rounded-lg"
-                                            layout="fill"
-                                            objectFit="cover"
-                                            src={vehicle.images?.length > 1 ? vehicle.images[1]?.url : ""}
-                                            alt="spec"
-                                        /> */}
+                        <div className="w-5/12">
+                            <div className=" flex flex-col rounded-xl" style={{boxShadow: '0px 0px 10px 1px rgba(0,0,0,0.2)'}}>
+                            
+                                <div className="flex flex-col ">
+                                    <div className="my-5 self-center font-bold text-xl tracking-wider">
+                                        <p>{vehicle.model?.brand?.name.toUpperCase()} {vehicle.model?.name.toUpperCase()}</p>
                                     </div>
-                                </div>
+                                    <hr className="border-2 rounded w-4/5 self-center" />
 
-                                <hr className="mt-5" />
+                                    <div>
+                                        <div className=" py-4 my-3 " >
+                                            <div className="flex justify-evenly text-primary opacity-90">
+                                                <div className="flex flex-col justify-between items-center my-2">
+                                                    <p className="font-bold">Year:</p>
+                                                    <p>{vehicle.year}</p>
+                                                </div>
+                                                <div className="flex flex-col justify-between items-center my-2">
+                                                    <p className="font-bold">Plate:</p>
+                                                    <p>{vehicle.plate}</p>
+                                                </div>
+                                                <div className="flex flex-col justify-between items-center my-2">
+                                                    <p className="font-bold">Price:</p>
+                                                    <p>${vehicle.price_per_day}<small className="text-xs">/day</small></p>
+                                                </div>
+                                            </div>
+                                        </div>
 
-                                <div className=" p-4 my-3 " >
-                                    <div className="flex justify-between">
-                                        <div className="flex flex-col justify-between items-center my-2">
-                                            <p className="font-medium">Year:</p>
-                                            <p>{vehicle.year}</p>
-                                        </div>
-                                        <div className="flex flex-col justify-between items-center my-2">
-                                            <p className="font-medium">Plate:</p>
-                                            <p>{vehicle.plate}</p>
-                                        </div>
-                                        <div className="flex flex-col justify-between items-center my-2">
-                                            <p className="font-medium">Price:</p>
-                                            <p>${vehicle.price_per_day}<small className="text-xs">/day</small></p>
+                                        <div>
+                                            <Slider {...settings} className="pb-20 " >
+                                                {vehicle.images?.length > 1 && vehicle.images?.length > 1 && vehicle.images.map(image => {
+                                                    return (<Image
+                                                        className=""
+                                                        width={550}
+                                                        height={400}
+                                                        src={image.url}
+                                                        alt="spec"
+                                                    />)
+                                                })}
+                                            </Slider>
+
                                         </div>
                                     </div>
-                                </div>
-                                <div>
-                                    <button className="bg-primary text-white font-bold rounded-xl px-2 py-2 m-2"
-                                        onClick={handleReservationClick}>   Confirm
-                                    </button>
                                 </div>
                             </div>
                         </div>
