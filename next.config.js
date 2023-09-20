cc6a8358cee195b370e58d90df1cbbd698cc4981
Castro <castrojonathand@gmail.com>
2023-09-20 19:20:26 -0300

    changes Next.config to deploy

--- next.config.js
+++ next.config.js
@@ -1,25 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    output: "export",
-    
-    
+const dom = ['https://www.com', 'upload.wikimedia.org', 'c6-pi-grupo3.s3.amazonaws.com', 'cdn.pixabay.com', 'assets.example.com', 'new-domain.com']
+const remPat = [
+  {
+    protocol: "https",
+    hostname: "assets.example.com",
+    port: "",
+    pathname: "via.placeholder.com",
+  }]
 
-    domains: [
-      'upload.wikimedia.org',
-      'c6-pi-grupo3.s3.amazonaws.com',
-      'cdn.pixabay.com',
-      'assets.example.com',
-    ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "assets.example.com",
-        port: "",
-        pathname: "via.placeholder.com",
-      },
-    ],
+/** @type {import('next').NextConfig} */
+const nextConfig = {  
+  // output: "export", 
+  
+  images: {
+    domains: dom,
+    remotePatterns: remPat,
+    unoptimized: true
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
